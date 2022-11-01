d874dbee82f1d049fdc7c970a67f609cc9e41302
MGabry98 <116531467+MohamedGabry98@users.noreply.github.com>
2022-11-01 11:58:07 +0200

    Final Commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD="12345678"
+export POSTGRES_HOST=database-2.cloptiwxhj9s.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udacityhubmgabry98
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://udagram-api-dev.eba-if3pqk8a.us-east-1.elasticbeanstalk.com:8100
